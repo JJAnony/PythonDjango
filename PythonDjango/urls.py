417d10fdb583c4e82676c0d61b7a59fcaa148940
JJAnony <jcolivati@gmail.com>
2021-07-11 21:02:19 -0300

    Created pages htmls

--- PythonDjango/urls.py
+++ PythonDjango/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from core import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/<name>/', views.hello)
+    path('hello/<name>/', views.hello),
+    path('schedule/', views.list_events),
+    path('', RedirectView.as_view(url='/schedule/'))
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render, HttpResponse
+from core.models import Event
+
 
 # Create your views here.
 
 def hello(requests, name):
     return HttpResponse('<h1>Hello {}</h1>'.format(name))
 
+
+def list_events(requests):
+    event = Event.objects.filter(user=requests.user)
+    response = {'events': event}
+    return render(requests, 'schedule.html', response)
